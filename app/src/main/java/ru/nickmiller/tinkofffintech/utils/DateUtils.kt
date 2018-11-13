7263e76314a9f6f633cb4c05c3c79942a06c5893
Nick Miller <nickmiller.on@gmail.com>
2018-11-13 20:44:29 +0300

    update some utils

---
+++ app/src/main/java/ru/nickmiller/tinkofffintech/utils/DateUtils.kt
@@ -0,0 +1,11 @@
+package ru.nickmiller.tinkofffintech.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+val sdfFintech = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
+
+
+ 
+ 
--- app/src/main/java/ru/nickmiller/tinkofffintech/utils/UIUtils.kt
+++ app/src/main/java/ru/nickmiller/tinkofffintech/utils/UIUtils.kt
@@ -1,10 +1,44 @@
 package ru.nickmiller.tinkofffintech.utils
 
+import android.animation.Animator
 import android.app.Activity
+import android.content.Context
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
+import android.util.TypedValue
 import android.view.View
+import android.view.ViewAnimationUtils
+import android.view.animation.AccelerateDecelerateInterpolator
 
 inline fun <reified V : View> Activity.find(id: Int): V = findViewById(id)
 
+inline fun <reified V : View> Fragment.find(id: Int): V? = view?.findViewById(id)
+
+inline fun <reified V : View> RecyclerView.ViewHolder.find(id: Int): V = itemView.findViewById(id)
+
 inline fun <reified V : View> View.find(id: Int): V = findViewById(id)
+
+
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+fun revealView(view: View, duration: Long, startRadius: Float): Animator {
+    val cx = view.measuredWidth
+    val cy = view.measuredHeight
+
+    val finalRadius = Math.max(view.width, view.height)
+
+    val anim = ViewAnimationUtils.createCircularReveal(view, cx, cy, startRadius, finalRadius.toFloat())
+    anim.duration = duration
+    anim.interpolator = AccelerateDecelerateInterpolator()
+
+    return anim
+}
+
+
+fun dpToPx(context: Context, valueInDp: Float): Float {
+    val metrics = context.resources.displayMetrics
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, valueInDp, metrics)
+}
  
  
