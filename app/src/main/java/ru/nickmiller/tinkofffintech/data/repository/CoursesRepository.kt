f89b569c1e390e83b69167e2b8473ed4b85475b7
Nick Miller <nickmiller.on@gmail.com>
2018-12-02 21:02:45 +0300

    updated Profile and Events repositories

--- app/src/main/java/ru/nickmiller/tinkofffintech/data/repository/CoursesRepository.kt
+++ app/src/main/java/ru/nickmiller/tinkofffintech/data/repository/CoursesRepository.kt
@@ -1,22 +1,79 @@
 package ru.nickmiller.tinkofffintech.data.repository
 
 import io.reactivex.Observable
+import io.reactivex.functions.Function3
+import ru.nickmiller.tinkofffintech.ConnectionIsNotAvailable
+import ru.nickmiller.tinkofffintech.EmptyDbTableException
 import ru.nickmiller.tinkofffintech.data.Api
 import ru.nickmiller.tinkofffintech.data.Resource
+import ru.nickmiller.tinkofffintech.data.cache.AppDatabase
+import ru.nickmiller.tinkofffintech.data.entity.course.Course
+import ru.nickmiller.tinkofffintech.data.entity.course.CourseAbout
+import ru.nickmiller.tinkofffintech.data.entity.course.CourseInfo
 import ru.nickmiller.tinkofffintech.utils.NetworkUtil
 
 
-class CoursesRepository(val api: Api, val networkUtil: NetworkUtil) {
+class CoursesRepository(val api: Api, val db: AppDatabase, val networkUtil: NetworkUtil) {
 
-    fun getCourses() = networkUtil.isConnected()
+    private fun courseInfoUrl(courseId: String) = "https://fintech.tinkoff.ru/api/course/$courseId/grades"
+    private fun courseAboutUrl(courseId: String) = "https://fintech.tinkoff.ru/api/course/$courseId/about"
+
+
+    fun getCourses(update: Boolean = false): Observable<Resource<List<Course>>> =
+        if (update) getCoursesFromCloud()
+        else {
+            Observable.concat(
+                getFromCache().filter { !it.data.isNullOrEmpty() },
+                getCoursesFromCloud()
+            )
+        }
+
+
+    fun getFromCache() = Observable.fromCallable {
+        val courses = db.coursesDao().getAllCourses()
+        if (courses != null) {
+            Resource.success(courses)
+        } else {
+            Resource.error(EmptyDbTableException())
+        }
+    }
+
+    fun getCoursesFromCloud() = networkUtil.isConnected()
         .flatMap { connected ->
             if (connected) {
-                api.getCourses().map {
-                    Resource.success(it)
-                }
+                api.getCourses().flatMapIterable { it.courses }
+                    .map { it.apply { userId = db.profileDao().getProfile()!!.id } }
+                    .flatMap { course ->
+                        Observable.zip(
+                            Observable.just(course),
+                            api.getCourseInfo(courseInfoUrl(course.url)),
+                            api.getCourseAbout(courseAboutUrl(course.url)),
+                            Function3<Course, List<CourseInfo>, CourseAbout, Course> { lCourse, courseInfo, courseAbout ->
+                                lCourse.courseInfo = courseInfo
+                                lCourse.courseAbout = courseAbout
+                                lCourse
+                            }
+                        )
+                    }
+                    .toList()
+                    .map {
+                        db.coursesDao().saveCourses(it)
+                        Resource.success(it)
+                    }
+                    .toObservable()
             } else {
-                Observable.just(Resource.error(RuntimeException("Отсутствует подключение к сети")))
+                Observable.just(Resource.error(ConnectionIsNotAvailable()))
             }
         }
 
+
+    fun getCourseInfo(courseId: String) = networkUtil.isConnected()
+        .flatMap { connected ->
+            if (connected) {
+                api.getCourseInfo("https://fintech.tinkoff.ru/api/course/$courseId/grades")
+                    .map { Resource.success(it) }
+            } else {
+                Observable.just(Resource.error(ConnectionIsNotAvailable()))
+            }
+        }
 }
--- app/src/main/java/ru/nickmiller/tinkofffintech/data/repository/ProfileRepository.kt
+++ app/src/main/java/ru/nickmiller/tinkofffintech/data/repository/ProfileRepository.kt
@@ -1,22 +1,57 @@
 package ru.nickmiller.tinkofffintech.data.repository
 
 import io.reactivex.Observable
+import ru.nickmiller.tinkofffintech.BaseException
+import ru.nickmiller.tinkofffintech.ConnectionIsNotAvailable
+import ru.nickmiller.tinkofffintech.InternalServerError
 import ru.nickmiller.tinkofffintech.data.Api
 import ru.nickmiller.tinkofffintech.data.Resource
+import ru.nickmiller.tinkofffintech.data.cache.AppDatabase
+import ru.nickmiller.tinkofffintech.data.entity.profile.Profile
 import ru.nickmiller.tinkofffintech.utils.NetworkUtil
+import java.util.concurrent.TimeUnit
 
 
-class ProfileRepository(val api: Api, val networkUtil: NetworkUtil) {
+class ProfileRepository(val api: Api, val db: AppDatabase, val networkUtil: NetworkUtil) {
 
-    fun getUserProfile() = networkUtil.isConnected()
-        .flatMap { connected ->
-            if (connected) {
-                api.getUserProfile()
-                    .flatMap {
-                        if (it.status == "Ok") Observable.just(Resource.success(it.user))
-                        else Observable.just(Resource.error(RuntimeException("Ошибка сервера")))
+    fun getUserProfile(refresh: Boolean = false): Observable<Resource<Profile>> =
+        if (refresh) {
+            getProfileFromCloud()
+        } else {
+            Observable.fromCallable {
+                val profile = db.profileDao().getProfile()
+                if (profile != null) Resource.success(profile)
+                else Resource.error(BaseException())
+            }
+                .flatMap { res ->
+                    if (res.data != null) {
+                        val minsFromLastUpdate =
+                            TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis() - res.data.lastUpdatedTime)
+                        if (minsFromLastUpdate >= 60) {
+                            Observable.concat(Observable.just(res), getProfileFromCloud())
+                        } else {
+                            Observable.just(res)
+                        }
+                    } else {
+                        getProfileFromCloud()
                     }
-            } else Observable.just(Resource.error(RuntimeException("Отсутствует подключение к сети")))
+                }
         }
 
+
+    private fun getProfileFromCloud(): Observable<Resource<Profile>> =
+        networkUtil.isConnected()
+            .flatMap { connected ->
+                if (connected) {
+                    api.getUserProfile()
+                        .flatMap {
+                            if (it.status == "Ok") {
+                                db.profileDao().saveProfile(it.user!!.apply {
+                                    lastUpdatedTime = System.currentTimeMillis()
+                                })
+                                Observable.just(Resource.success(it.user))
+                            } else Observable.just(Resource.error(InternalServerError()))
+                        }
+                } else Observable.just(Resource.error(ConnectionIsNotAvailable()))
+            }
 }
