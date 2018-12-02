05d7054a2ef92bc943caf839efcd56c2ae3e018b
Nick Miller <nickmiller.on@gmail.com>
2018-12-02 21:07:15 +0300

    implement LayoutContainer in BaseViewHolder

--- app/src/main/java/ru/nickmiller/tinkofffintech/ui/base/BaseViewHolder.kt
+++ app/src/main/java/ru/nickmiller/tinkofffintech/ui/base/BaseViewHolder.kt
@@ -2,8 +2,12 @@ package ru.nickmiller.tinkofffintech.ui.base
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import kotlinx.android.extensions.LayoutContainer
 
-abstract class BaseViewHolder<T>(itemView: View): RecyclerView.ViewHolder(itemView) {
+abstract class BaseViewHolder<T>(itemView: View) :
+    RecyclerView.ViewHolder(itemView), LayoutContainer {
+
+    override val containerView = itemView
     abstract fun bind(item: T)
 }
  
